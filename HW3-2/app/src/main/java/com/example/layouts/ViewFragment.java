65516fd21c54965e4de747226a0df5c282ce5bc3
Kevin Law <kevin@Thaumaturge-Pro.local>
2019-06-05 00:27:06 -0700

    hw 3 done

---
+++ HW3-2/app/src/main/java/com/example/layouts/ViewFragment.java
@@ -0,0 +1,52 @@
+package com.example.layouts;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.content.Intent;
+
+public class ViewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        RecyclerView movieRecycler = (RecyclerView)inflater.inflate(R.layout.fragment_view, container, false);
+
+        String[] movieTitles = new String[Movie.movie.length];
+        for (int titles = 0; titles < movieTitles.length; titles++){
+            movieTitles[titles]  = Movie.movie[titles].getTitle();
+        }
+
+        int[] movieImages = new int[Movie.movie.length];
+        for (int imageId = 0; imageId < movieImages.length; imageId++) {
+            movieImages[imageId] = Movie.movie[imageId].getImageId();
+        }
+        int[] movieYears = new int[Movie.movie.length];
+        for (int year = 0; year < movieYears.length; year++) {
+            movieYears[year] = Movie.movie[year].getYear();
+        }
+
+        MovieImageAdapter adapter = new MovieImageAdapter(movieTitles, movieImages, movieYears);
+
+        movieRecycler.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        movieRecycler.setLayoutManager(layoutManager);
+
+        adapter.setListener(new MovieImageAdapter.Listener() {
+            @Override
+            public void onClick(int position) {
+                Intent intent = new Intent(getActivity(), MovieDetailActivity.class);
+                intent.putExtra(MovieDetailActivity.MOVIE_ID, position);
+                getActivity().startActivity(intent);
+            }
+        });
+
+
+        return movieRecycler;
+    }
+}
