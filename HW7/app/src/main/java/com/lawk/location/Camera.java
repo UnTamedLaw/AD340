da0add19d35e915692f8640b17035a89626ff04a
Kevin Law <kevin@Thaumaturge-Pro.local>
2019-06-06 11:38:31 -0700

    base hw7 files

---
+++ HW7/app/src/main/java/com/lawk/location/Camera.java
@@ -0,0 +1,25 @@
+package com.lawk.location;
+
+public class Camera {
+    private String cameraName;
+    private String imageURL;
+    private String type;
+
+    public Camera(String cameraName, String imageURL, String type) {
+        this.cameraName = cameraName;
+        this.imageURL = imageURL;
+        this.type = type;
+    }
+
+    public String getCameraName() {
+        return cameraName;
+    }
+    public String getImageURL() {
+        return imageURL;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+}
